9ea61b8ea99026c6ba92275173d6351f74a4b707
Bapi Gupta <rajat310198@outlook.com>
2018-03-03 17:41:08 +0530

    Skeleton of blockchain class

---
+++ src/blockchain.py
@@ -0,0 +1,43 @@
+'''
+
+blockchain.py
+Implementation of Blockchain and its methods
+
+Author: Rajat Gupta
+Email: rajat15101@iiitnr.edu.in
+Time & Date: 17:32 hrs, Sat, 3trd March 2018
+
+'''
+
+import warnings, constants
+from pyp2p import *
+from transaction import *
+from block import Block
+
+
+
+class Blockchain(object):
+    '''
+
+    Chain of blocks will be blockchain.
+    The object of this class stores individual block object of Block class. The longer the chain more authenticity.
+    
+    '''
+
+    def __init__(self):
+        pass
+
+    def return_last_block(self):
+        pass
+
+    def return_genesis_block(self):
+        pass
+
+    def initialize_genesis(self):
+        pass
+
+    def lenght_of_chain(self):
+        pass
+
+    def add_block(self, block):
+        pass
