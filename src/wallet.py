a6932697b7a360a21402197dbad417ef3cb30885
Bapi Gupta <rajat310198@outlook.com>
2018-05-02 18:46:21 +0530

    Wallet added

---
+++ src/wallet.py
@@ -0,0 +1,56 @@
+#client/peers for voting
+import socket, os, sys
+from .block import Block
+from hashlib import sha256
+from .transaction import Transaction
+import time, pickle
+
+
+public_list_of_peers = [
+    ("0.0.0.0", 10009, "active")
+]
+class User:
+    def __init__(self, name, age, publickey, privatekey, candidate=False):
+        self.name = name 
+        self.age = age
+        self.candidate = candidate
+        self.publickey = publickey
+        self.privatekey = privatekey
+
+
+
+def register(sock):
+    global public_list_of_peers
+    name = input("Enter Name")
+    age = input("Enter age")
+    if int(age)>17:
+        user = User(name, age, sha256(name.encode() + str(age).encode()).hexdigest(), sha256(name.encode() + str(age).encode()).hexdigest(), candidate=False)
+        to = input("Enter digital signature of candidate you wanna vote :")
+        
+
+        new_Transaction = Transaction(to, user.publickey)
+        print(user.publickey)
+        new_Transaction.timestamp = time.ctime()
+        new_Transaction.digital_signature = sha256(to.encode() + user.privatekey.encode()).hexdigest()
+        print(new_Transaction,'Digital Signature : ', new_Transaction.digital_signature)
+    else:
+        sock.close()
+
+        
+def connectToNetwork():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    sock.connect(('0.0.0.0', 10009))
+    sock.send('wallet'.encode())
+    
+    register(sock)
+
+    data = sock.recv(1024)
+    public_list_of_peers = pickle.loads(data)
+    print(public_list_of_peers)
+
+    
+    
+        
+
+#register()
